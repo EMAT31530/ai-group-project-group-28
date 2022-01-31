e35afc77ba1df0a759de90a09ec3f10121193083
larawarwick <83228846+larawarwick@users.noreply.github.com>
2022-01-31 22:25:57 +0000

    KNN_V1
    Doesn't require data to be indexed

--- KNN.py
+++ KNN.py
@@ -1,42 +1,40 @@
-from collections import Counter
 import math
+from collections import Counter
 
-def mode(labels):
-    return Counter(labels).most_common(1)[0][0]
-
-def euclidean_distance(point1, point2):
-    sum_squared_distance = 0
-    for i in range(len(point1)):
-        sum_squared_distance += math.pow(point1[i] - point2[i], 2)
-    return math.sqrt(sum_squared_distance)
-
-def knn(data, newpoint, k, distance_fn):
-    neighbor_distances_and_indices = []
-    
-    # For each example in the data
-    for index, example in enumerate(data):
-        # Calculate the distance between the newpoint example and the current
-        # example from the data.
-        distance = euclidean_distance(example[:-1], newpoint) 
-        # example slice is all up until one before the end
-        # takes two points and returns the distance between them using Euclidean Metric
+def knn(data, newpoint, k): 
+    # Data shouldn't be indexed 
+    # We want to be able to test knn on different values of k so we add it as a parameter
+    neighbour_dists = [] # with indices
+    # For each point in the data we...
+    for index, point in enumerate(data):
+        # Calculate the distance between the newpoint and the current
+        # point from the data.
+        distance = 0
+        for i in range(len(point[:-1])):
+            distance = distance + math.pow(point[:-1][i] - newpoint[i], 2)
+            distance = math.sqrt(distance)
+        # Last index is the label so we don't include it in this calculation
+        # Takes two points and calculates the distance between them using Euclidean Metric
         
-        # Add the distance and the index of the example to an ordered collection
-        neighbor_distances_and_indices.append((distance, index))
+        # Add the distance and the index to neighbour_dists
+        neighbour_dists.append((distance, index))
     
-    # Sort the ordered collection of distances and indices from
-    # smallest to largest (in ascending order) by the distances
-    sorted_neighbor_distances_and_indices = sorted(neighbor_distances_and_indices)
+    # Sort (in ascending order)
+    sorted_neighbour_dists = sorted(neighbour_dists)
     
-    # Pick the first K entries from the sorted collection
-    k_nearest_distances_and_indices = sorted_neighbor_distances_and_indices[:k]
+    # Pick the k closest points
+    k_nearest_dists_and_indices = sorted_neighbour_dists[:k]
     
-    # Get the labels of the selected K entries
-    k_nearest_labels = [data[i][-1] for distance, i in k_nearest_distances_and_indices]
+    # Get the labels of the k selected points 
+    k_nearest_labels = [data[i][-1] for distance, i in k_nearest_dists_and_indices]
 
-    return k_nearest_distances_and_indices , mode(k_nearest_labels)
+    return Counter(k_nearest_labels).most_common(1)[0][0]
+    # Returns the predicted label for our newpoint
+    # We want the 1st most common and the zeroth element as we want the most
+    # common label, not the amount of times it has occured (which is index 1)
 
-
-
+# Test
+Data = [[1,1], [2,-1], [4,-1], [8,1], [5,1], [2,1]]
+print(knn(Data, [6], 2))
 
 
