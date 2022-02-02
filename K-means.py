1e35b628d95a42e9411d2804778749c55ee936e9
larawarwick <83228846+larawarwick@users.noreply.github.com>
2022-02-02 10:27:01 +0000

    Update K-means.py
    We now have an array of each point and the index of their closest centroid

--- K-means.py
+++ K-means.py
@@ -11,9 +11,11 @@ def k_means(data, newpoint, k):
         else:
             continue
     # There is definitely a more efficient way to do this ^
-    print(centroids)
     # For each point calculate the distance it is from all the centroids
     # and assign the point to the centroid it is closest to
+    # We make the arrays for the centroids which starts with each 
+    # centroid having no points assigned to it
+    assigned_centroids = []
     for point in data:
         dist_from_centroids = [] # Includes indexs
         for index, centroid in enumerate(centroids):
@@ -23,11 +25,13 @@ def k_means(data, newpoint, k):
             distance = math.sqrt(distance)
             dist_from_centroids.append((distance, index))
             sorted_dist_from_centroids = sorted(dist_from_centroids)
-            #sorted_dist_from_centroids[0]
-            #nearest_centroid = max(dist_from_centroid)
-        print(sorted_dist_from_centroids)
-        print('\n')
-
+            #print(sorted_dist_from_centroids)
+            #print(sorted_dist_from_centroids[0][1])
+            closest_centroid_index = sorted_dist_from_centroids[0][1]
+            # We want our index of the first distance in the list^
+        assigned_centroids.append((point, closest_centroid_index))
+        # This is now our array of [([point], index), ...] where the index is the index of the centroid
+    print(assigned_centroids)
 Data = [[1,8], [2,-14], [4,-1], [8,7], [5,-5], [2,3]]
 
 print(k_means(Data, [5,6], 4))
